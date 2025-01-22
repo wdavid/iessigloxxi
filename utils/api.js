7f0c669586bf2b301830115c066b36c8b2e9643f
wdavid <00135222@uca.edu.sv>
2025-01-22 16:51:59 +0100

    metadata IES

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3500/api" || process.env.NEXT_API_URL, 
+  baseURL: process.env.NEXT_API_URL, 
 });
 
 export default api;
